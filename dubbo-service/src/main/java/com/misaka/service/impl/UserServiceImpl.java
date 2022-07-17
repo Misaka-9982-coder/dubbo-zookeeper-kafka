f80a8911fdeab083ff32934e14ccb02e4cbb5fc8
misaka <1784321894@qq.com>
2022-07-17 21:21:34 +0800

    Dubbo : Timeout Retries

--- dubbo-service/src/main/java/com/misaka/service/impl/UserServiceImpl.java
+++ dubbo-service/src/main/java/com/misaka/service/impl/UserServiceImpl.java
@@ -4,14 +4,16 @@ import com.misaka.pojo.User;
 import com.misaka.service.UserService;
 import org.apache.dubbo.config.annotation.Service;
 
-@Service(timeout = 3000, retries = 0)
+@Service(timeout = 3000, retries = 2)
 public class UserServiceImpl implements UserService {
     public String sayHello() {
         return "Hello world!";
     }
 
+    int i = 1;
     @Override
     public User getUserById(int id) {
+        System.out.println("================= " + i ++ + " =================");
         User user = new User(id, "misaka" + id, "123456" + id);
 
         try {
