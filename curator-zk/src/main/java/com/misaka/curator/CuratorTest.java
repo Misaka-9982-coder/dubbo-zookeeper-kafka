052e46eeaddc76f65c7b255ae84f710b00150691
misaka <1784321894@qq.com>
2022-07-18 14:17:13 +0800

    Zookeeper JavaAPI : Create Node

--- curator-zk/src/main/java/com/misaka/curator/CuratorTest.java
+++ curator-zk/src/main/java/com/misaka/curator/CuratorTest.java
@@ -5,23 +5,28 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CuratorTest {
 
-    @Test
+    CuratorFramework build;
+
+    @Before
     public void testConnect() {
 
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(3000, 10);
-
-        // first method
-        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("localhost:2181",
-                60 * 1000, 15 * 1000, retryPolicy);
-        curatorFramework.start();
+//
+//        // first method
+//        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("localhost:2181",
+//                60 * 1000, 15 * 1000, retryPolicy);
+//        curatorFramework.start();
 
 
         // second method
-        CuratorFramework build = CuratorFrameworkFactory.builder()
+        build = CuratorFrameworkFactory.builder()
                 .connectString("localhost:2181")
                 .sessionTimeoutMs(60 * 1000)
                 .connectionTimeoutMs(15 * 1000)
@@ -30,5 +35,36 @@ public class CuratorTest {
                 .build();
 
         build.start();
+    }
+
+    @Test
+    public void testCreate1() throws Exception {
+        String path = build.create().forPath("/app1");
+        System.out.println(path);
+    }
+
+    @Test
+    public void testCreate2() throws Exception {
+        String path = build.create().forPath("/app2", "hello".getBytes());
+        System.out.println(path);
+    }
+
+    @Test
+    public void testCreate3() throws Exception {
+        String path = build.create().withMode(CreateMode.EPHEMERAL).forPath("/app3");
+        System.out.println(path);
+    }
+
+    @Test
+    public void testCreate4() throws Exception {
+        String path = build.create().creatingParentContainersIfNeeded() .forPath("/app4/p1");
+        System.out.println(path);
+    }
+
+    @After
+    public void testClose() {
+        if(build != null) {
+            build.close();
+        }
     }
 }
